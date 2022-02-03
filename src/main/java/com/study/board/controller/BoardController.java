9617832862bba55af22dddad58bc447e9bdf63a6
dgjinsu <rlawlstn3206@naver.com>
2022-02-03 12:35:05 +0900

    simple_board

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -3,6 +3,10 @@ package com.study.board.controller;
 import com.study.board.entitiy.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,16 +47,34 @@ public class BoardController {
     //Model 은 HashMap의 형태를 띄고있다.
     //addAttribute를 통해 모델에 원하는 속성과 그것에 대한 값을 주어 전달할 뷰에 데이터를 전송할 수 있다.
 
+    //리스트에 페이징처리
     @GetMapping("/board/list")
-    public String boardlist(Model model){
-        //게시글을 전부 리턴받은걸 list라는 이름으로 전부 보냄
-        model.addAttribute("list", boardService.boardList());
-        //보낸걸 boardlist.html에서 받음
+    public String boardlist(Model model,
+                            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+                            String searchKeyword){
+        Page<Board> list = null;
+
+        if(searchKeyword == null){
+            list = boardService.boardList(pageable);
+        }
+        else{
+            list = boardService.boardSearchList(searchKeyword, pageable);
+        }
+
+        int nowPage = list.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+
+        //게시글을 전부 리턴받은걸 list라는 이름으로 전부 보냄        //보낸걸 boardlist.html에서 받음
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         return "boardlist";
     }
 
-    @GetMapping("/board/view") //localhost:8080/board/view?id=1
-    public String boardView(Model model, Integer id){
+    @GetMapping("/board/view/{id}") //localhost:8080/board/view?id=1
+    public String boardView(Model model, @PathVariable("id") Integer id){
         model.addAttribute("board", boardService.boardView(id));
         return "boardview";
     }
